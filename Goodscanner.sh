adc73e0af3b55b04d9a02bec21f6260187895ec0
Pol8y <64278895+Pol8y@users.noreply.github.com>
2022-12-20 12:46:13 +0100

    Update Goodscanner.sh

--- Goodscanner.sh
+++ Goodscanner.sh
@@ -8,7 +8,7 @@ then
 fi
 
 # Scan the IP address using the -T4 and -vv options for maximum speed and verbosity
-open_ports=$(nmap -T4 -A -vv -oG sweep."$1".txt -p- "$1" | grep "open port" | cut -d'/' -f1 | cut -d" " -f4 | tr '\n' ',' | sed 's/,$//'
+open_ports=$(nmap -T4 -vv -oG sweep."$1".txt -p- "$1" | grep "open port" | cut -d'/' -f1 | cut -d" " -f4 | tr '\n' ',' | sed 's/,$//'
 
 # Enumerate the services, Os, and script scan on the open ports using the -A option and -p "$open_ports" to scan only open ports.
 nmap -A -vv -oG scan."$1".txt -p "$open_ports" "$1"
