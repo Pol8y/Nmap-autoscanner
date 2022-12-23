4936c38edf8e51570e3e9cafd2e67abac365f23a
Pol8y <64278895+Pol8y@users.noreply.github.com>
2022-12-23 02:16:58 +0100

    Update Goodscanner.sh

--- Goodscanner.sh
+++ Goodscanner.sh
@@ -7,6 +7,13 @@ then
     echo "Please provide an IP address as an argument."
     exit 1
 fi
+# Check if -Pn option was provided
+if [ "$2" == "-Pn" ]
+then
+    pn_option="-Pn"
+else
+    pn_option=""
+fi
 # Use a regular expression to check if the input is a valid IP address
 if [[ $ip_address =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
   # Split the IP address into its octets
@@ -30,7 +37,7 @@ echo "Initial Scan Started on: "$timestamp" -- waiting for ports"
 # Scan the IP address using the -T4, and -vv options for maximum speed and verbosity, then grep lines that contains "open ports", cut after /, 
 # cut the fourth column that contains the port, translate newlines to commas, and finally remove commas from the end of the lines, leaving nothig but port number.
 
-open_ports=$(nmap -T4 -vv -oG sweep."$ip_address".txt -p- "$ip_address" | grep "open port" | cut -d'/' -f1 | cut -d" " -f4 | tr '\n' ',' | sed 's/,$//')
+open_ports=$(nmap $pn_option -T4 -vv -oG sweep."$ip_address".txt -p- "$ip_address" | grep "open port" | cut -d'/' -f1 | cut -d" " -f4 | tr '\n' ',' | sed 's/,$//')
 
 # Check if open_ports is empty
 if [ -z $open_ports ]
@@ -46,7 +53,7 @@ elapsedtime=$((t2-t1))
 echo "Initial Scan completed in $elapsedtime seconds"
 
 # Enumerate the services, Os, and script scan on the open ports using the -A option and -p "$open_ports" to scan only open ports.
-nmap -A -vv -oG scan."ip_address".txt -p "$open_ports" "$ip_address"
+nmap $pn_option -A -vv -oG scan."ip_address".txt -p "$open_ports" "$ip_address"
 echo -e "\n"
 t3=$(date +%s)
 elapsedtime=((t3-t2))
